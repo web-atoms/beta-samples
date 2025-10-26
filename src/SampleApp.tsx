b9f5048b48af8693cac4dbd52b8f76985977aa17
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-10-26 16:13:13 +0530

    wip

---
+++ src/SampleApp.tsx
@@ -0,0 +1,34 @@
+import { App } from "@web-atoms/web-atoms/dist/App";
+import { AtomControl } from "@web-atoms/web-atoms/dist/core/AtomControl.js";
+import Bind from "@web-atoms/web-atoms/dist/core/Bind.js";
+import XNode from "@web-atoms/web-atoms/dist/core/XNode.js";
+
+export default class SampleApp extends AtomControl {
+
+    firstName = "";
+    lastName = "";
+
+    constructor(app, e) {
+        super(app, e);
+        this.pushInit();
+    }
+
+    async init() {
+        this.renderer = <div>
+            <div>
+                <input placeholder="First Name" value={Bind.twoWaysImmediate(() => this.firstName)}/>
+            </div>
+            <div>
+                <input placeholder="Last Name" value={Bind.twoWaysImmediate(() => this.lastName)}/>
+            </div>
+            <div>
+                <div>Full name is <span text={Bind.oneWay(() => `${this.firstName} ${this.lastName}`)} /></div>
+            </div>
+        </div>;
+    }
+
+}
+
+const webApp = new App();
+const sample = new SampleApp(webApp, document.createElement("main"));
+document.body.append(sample.element);
